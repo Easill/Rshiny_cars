b56d46968dc52deac996aa9a75143777f201ada2
Easill <90337662+Easill@users.noreply.github.com>
2021-10-01 13:46:02 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,25 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    res_pca <- reactive({
+        PCA(cars[,input$ACPCheck], quali.sup = input$qualiCheck, graph = FALSE)
+    })
+
+    output$graph_pca_ind <- renderPlot({
+        plot.PCA(res_pca(), choix = "ind", axes = c(1,2))
+    })
+    output$graph_pca_var <- renderPlot({
+        plot.PCA(res_pca(), choix = "var", axes = c(1,2))
+    })
+})
